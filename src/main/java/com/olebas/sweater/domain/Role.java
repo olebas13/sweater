d2ac3ee382c773b8b40762e3bbcbbca0eb53bce4
olebas13 <o.nevoyt13@gmail.com>
2019-04-23 17:13:27 +0300

    add registration and autorization

---
+++ src/main/java/com/olebas/sweater/domain/Role.java
@@ -0,0 +1,5 @@
+package com.olebas.sweater.domain;
+
+public enum Role {
+    USER;
+}
---
+++ src/main/java/com/olebas/sweater/repository/UserRepo.java
@@ -0,0 +1,9 @@
+package com.olebas.sweater.repository;
+
+import com.olebas.sweater.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User, Long> {
+
+    User findByUsername(String username);
+}
